96b7e61187a90558fcaba45927b7b258dcad11b7
nourimanov <nourimanov@gmail.com>
2023-10-18 11:38:48 +0500

    success

--- apps/views.py
+++ apps/views.py
@@ -1,6 +1,4 @@
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth.hashers import make_password
 from django.core.mail import send_mail
 from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
